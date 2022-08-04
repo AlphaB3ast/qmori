8cefe0d45ca396c8df1f6c84ac9942bd918efab2
DataWi <wi21b094@technikum-wien.at>
2022-08-04 03:07:29 +0200

    Theme set up and wrapped application with it

---
+++ utils/styling/theme.tsx
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+// APPLY GUIDE FROM https://dev-yakuza.posstree.com/en/react/nextjs/mui/
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff8e88",
+    },
+  },
+});
